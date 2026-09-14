0c31ecc52800170a97aa01b157134176b06a7485
Vladislav <nbdart@yandex.ru>
2026-09-14 12:28:31 +0500

    Небольшой фикс перемещения фигур
    Сдвинулся коллайдер после чистки проекта, поправил.

--- Assets/Scripts/Runtime/PawnsManager.cs
+++ Assets/Scripts/Runtime/PawnsManager.cs
@@ -66,7 +66,7 @@ namespace CrazyPawn
         {
             _connectionsManager.CancelPendingConnection();
             _dragOffset = _worldPlane.TryGetPointOnPlaneFromMousePosition(out Vector3 point) 
-                ? pawn.transform.position - point 
+                ? pawn.GetCenter() - point 
                 : Vector3.zero;
         }
 
